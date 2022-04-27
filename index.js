8ba858c6b0acf17de943e052ae11925dc68b07b5
Fernando Zablah <zablah.developer@gmail.com>
2022-04-27 17:14:35 -0500

    Fixed merge conflicts

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ log('hola mundo 2');
 const sacarPromedio = (nota1, nota2, nota3) => {
     return (nota1 + nota2 + nota3) / 3;
 }
+
+const generarReporte = () => {
+    console.log('Generando reporte...');
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+
+    }
+}
