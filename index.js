8d7bb82a2e3dfc35094c9f73662808a9860d72f1
Fernando Zablah <zablah.developer@gmail.com>
2022-04-27 17:12:15 -0500

    Added sacarpromedio

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 console.log('hola mundo');
 console.log('Mi contrasena es: 123456');
 log('hola mundo 2');
+
+
+const sacarPromedio = (nota1, nota2, nota3) => {
+    return (nota1 + nota2 + nota3) / 3;
+}
